c60310c85cd35f20cc25ebb86e94993dc5f6a6d4
Sergio Portoles Diez <sergio.portoles.diez@intermodalics.eu>
2025-06-10 16:08:08 +0200

    add std_srvs conversions for service "Empty"
    This patch adds a new conversion support for the
    "Empty" service of package std_srvs.

---
+++ include/ros2in1_support/conversions/std_srvs.h
@@ -0,0 +1,44 @@
+/******************************************************************************
+ *  Copyright (c) 2025, Intermodalics BVBA                                    *
+ *  All rights reserved.                                                      *
+ ******************************************************************************/
+
+#ifndef ROS2IN1_SUPPORT_CONVERSIONS_STD_SRVS_H
+#define ROS2IN1_SUPPORT_CONVERSIONS_STD_SRVS_H
+#ifdef ROS2_SUPPORT
+
+#include <utility>
+
+#include <std_srvs/srv/empty.hpp>
+#include <std_srvs/Empty.h>
+
+#include "builtin_interfaces.h"
+
+namespace ros2in1_support {
+namespace conversions {
+
+template <typename Ros1ServiceT> struct Ros2ServiceType;
+
+template <> struct Ros2ServiceType<std_srvs::Empty> {
+  typedef std_srvs::srv::Empty type;
+};
+
+template <>
+inline void convert_2_to_1<std_srvs::Empty::Request, std_srvs::srv::Empty::Request>(
+    const std_srvs::srv::Empty::Request&,
+    std_srvs::Empty::Request&) {
+  // empty
+}
+
+template <>
+inline void convert_1_to_2<std_srvs::srv::Empty::Response, std_srvs::Empty::Response>(
+    std_srvs::Empty::Response&&,
+    std_srvs::srv::Empty::Response&) {
+  // empty
+}
+
+}  // namespace conversions
+}  // namespace ros2in1_support
+
+#endif  // ROS2_SUPPORT
+#endif  // ROS2IN1_SUPPORT_CONVERSIONS_STD_SRVS_H
